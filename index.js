5fda35abefb5be254da8ffc7ec9c39411768ca66
Nickon Fath-Ordoubadi <nickonfo@gmail.com>
2018-07-02 13:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,7 @@ function sayHiToGrandma(string) {
   
   if (string.toLowerCase())
     return "I can't hear you"
+  else if (string.toUpperCase())
+    return "YES INDEED"
+  else if (string === "")  
 }
